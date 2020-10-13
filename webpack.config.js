6a8af5dceb85560d30a3b8359e3f1b7d45d89011
Mahdi <mahditahavorgar@gmail.com>
2020-10-13 16:43:57 +0330

    setup test databes

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,16 @@
 const path = require('path');
-const webpack = require('webpack')
+const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const webpack = require('webpack');
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if (process.env.NODE_ENV === 'test') {
-  require('dotenv').config({path: '.env.test' })
+  require('dotenv').config({ path: '.env.test' });
 } else if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config({path: '.env.development' })
+  require('dotenv').config({ path: '.env.development' });
 }
+
 
 // process.env.NODE_ENV 
 
